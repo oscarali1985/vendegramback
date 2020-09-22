dcbdb2d950a1e5cc7b60f9f7281e9bb19921c30f
oscarali1985 <oscarali1985@hotmail.com>
2020-09-22 15:15:17 -0400

    Se agrega el modelo de usuario y el GET y post del mismo

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuario
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,88 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/usuario", methods=["GET", "POST"])
+def cr_usuario():
+    """
+        "GET": devolver lista de todos los donantes
+        "POST": crear un donante y devolver su información
+    """
+    # averiguar si es GET o POST
+    if request.method == "GET":
+        #   seleccionar todos los registros de la tabla donantes - usando flask-sqlalchemy
+        #   crear una variable lista y asignarle todos los donantes que devuelva la consulta
+        usuarios = Usuario.query.all()
+        # verificamos si hay parámetros en la url y filtramos la lista con eso
+        name = request.args.get("name")
+        if name is not None:
+            usuarios_filtrados = filter(lambda usuario: name.lower() in usuario.nombre_completo.lower(), usuarios)
+        else:
+            usuarios_filtrados = usuarios
+        #   serializar los objetos de la lista - tendría una lista de diccionarios
+        usuarios_serializados = list(map(lambda usuario: usuario.serializar(), usuarios_filtrados))
+        print(usuarios_serializados)
+        #   devolver la lista jsonificada y 200_OK
+        return jsonify(usuarios_serializados), 200
+    else:
+        #   crear una variable y asignarle diccionario con datos para crear donante
+        dato_reg = request.json # request.get_json()
+        if dato_reg is None:
+            return jsonify({
+                "resultado": "no envió insumos para crear el donante..."
+            }), 400
+        #   verificar que el diccionario tenga cedula, nombre, apellido
+        if (
+            "nombre" not in dato_reg or
+            "apellido" not in  dato_reg or
+            "nombre_usuario" not in dato_reg or
+            #"fecha_nacimiento"not in dato_reg or
+            "correo" not in dato_reg or
+            "telefono" not in dato_reg or
+            "clave" not in dato_reg
+        ):
+            return jsonify({
+                "resultado": "revise las propiedades de su solicitud"
+            }), 400
+        #   validar que campos no vengan vacíos y que cédula tenga menos de 14 caracteres
+        if (
+            dato_reg["nombre"] == "" or
+            dato_reg["apellido"] == "" or
+            dato_reg["nombre_usuario"] == ""
+            #len(str(insumos_donante["cedula"])) > 14
+        ):
+            return jsonify({
+                "resultado": "revise los valores de su solicitud"
+            }), 400
+        #   crear una variable y asignarle el nuevo donante con los datos validados
+        
+        nuevo_usuario = Usuario.registrarse(
+            dato_reg["nombre"],
+            dato_reg["apellido"],
+            dato_reg["nombre_usuario"],
+            dato_reg["fecha_nacimiento"],
+            dato_reg["correo"],
+            dato_reg["telefono"],
+            dato_reg["clave"],
+            dato_reg["foto_perfil"],
+            dato_reg["administrador"],
+            dato_reg["suscripcion"]
+        )
+        
+        #   agregar a la sesión de base de datos (sqlalchemy) y hacer commit de la transacción
+        db.session.add(nuevo_usuario)
+        try:
+            db.session.commit()
+            # devolvemos el nuevo donante serializado y 201_CREATED
+            return jsonify(nuevo_usuario.serializar()), 201
+        except Exception as error:
+            db.session.rollback()
+            print(f"{error.args} {type(error)}")
+            # devolvemos "mira, tuvimos este error..."
+            return jsonify({
+                "resultado": f"{error.args}"
+            }), 500
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,108 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column(db.String(50), unique=False, nullable=False)
+    apellido = db.Column(db.String(50), unique=False, nullable=False)
+    nombre_usuario = db.Column(db.String(20), unique=True, nullable=False)
+    fecha_nacimiento = db.Column(db.DateTime(timezone=True))
+    correo = db.Column(db.String(50), unique=True, nullable=False)
+    telefono= db.Column(db.String(20), unique=False, nullable=False)
+    clave_hash = db.Column(db.String(80), unique=False, nullable=False)
+    foto_perfil = db.Column(db.String(120), unique=False, nullable=False)
+    administrador = db.Column(db.Boolean(), unique=False, nullable=False)
+    suscripcion = db.Column(db.Integer, unique=False, nullable=True)
+
+    def __init__(self, nombre, apellido, nombre_usuario, fecha_nacimiento, correo, telefono, clave_hash, foto_perfil, administrador, suscripcion):
+        """ crea y devuelve una instancia de esta clase """
+        self.nombre = nombre
+        self.apellido = apellido
+        self.nombre_usuario = nombre_usuario
+        self.fecha_nacimiento = fecha_nacimiento
+        self.correo = correo
+        self.telefono = telefono
+        self.clave_hash = clave_hash
+        self.foto_perfil = foto_perfil
+        self.administrador = administrador
+        self.suscripcion = suscripcion
+
+    def __str__(self):
+        return f"\t{self.id} ->  {self.nombre_completo}"
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Usuario %r>' % self.correo
 
-    def serialize(self):
+    @classmethod
+    def registrarse(cls, nombre, apellido, nombre_usuario, fecha_nacimiento, correo, telefono, clave, foto_perfil, administrador, suscripcion):
+        """
+            normaliza insumos nombre y apellido,
+            crea un objeto de la clase Donante con
+            esos insumos y devuelve la instancia creada.
+        """
+        nuevo_usuario = cls(
+            nombre.lower().capitalize(),
+            apellido.lower().capitalize(),
+            nombre_usuario,
+            fecha_nacimiento,
+            correo.casefold(),
+            telefono,
+            clave,
+            foto_perfil,
+            administrador,
+            suscripcion
+        )
+        return nuevo_usuario
+
+    @property
+    def nombre_completo(self):
+        """ devuelve nombre + ' ' apellido """
+        return f"{self.nombre} {self.apellido}"
+
+    def serializar(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "nombre": self.nombre,
+            "apellido":self.apellido,
+            "nombre_usuario":self.nombre_usuario,
+            "fecha_nacimiento":self.fecha_nacimiento, 
+            "correo":self.correo,
+            "telefono":self.telefono,
+            #La clave no se serializa,
+            "foto_perfil":self.foto_perfil,
+            "suscripcion":self.suscripcion 
+            "administrador":self.administrador
         }
+
+
+    def actualizar_usuario(self, diccionario):
+        """ actualiza propiedades del usuario según el contenido del diccionario """
+
+        if "email" in diccionario:
+            self.email = diccionario["email"]
+        if "nombre" in diccionario:
+            self.nombre = diccionario["nombre"]
+        if "apellido" in diccionario:
+            self.apellido = diccionario["apellido"]
+        if "nombre_usuario" in diccionario:
+            self.nombre_usuario = diccionario["nombre_usuario"]
+        if "fecha_nacimiento" in diccionario:
+            self.fecha_nacimiento = diccionario["fecha_nacimiento"]
+        if "correo" in diccionario:
+            self.correo = diccionario["correo"]
+        if "telefono" in diccionario:
+            self.telefono = diccionario["telefono"]
+        if "clave" in diccionario:
+            self.clave_hash = diccionario["clave"]
+        if "foto_perfil" in diccionario:
+            self.foto_perfil = diccionario["foto_perfil"]
+        if "suscripcion" in diccionario:
+            self.suscripcion = diccionario["suscripcion"]    
+
+        # for (key, value) in diccionario.items():
+        #     if hasattr(self, key) and key != "cedula":
+        #         self[key] = value
+        return True
